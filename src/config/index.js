b72948e44247d07ee4bdc0e5c7e83b1faa78bd5d
florence <loracodes3@gmail.com>
2024-11-12 00:39:53 +0300

    backend

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 const config = {
-    BASE_URL: 'http://localhost:6001'
+    // BASE_URL: 'http://localhost:6001'
+     BASE_URL: "https://react-hooks-cc-plantshop-wtdl.onrender.com"
 }
 
 export default config
